e29a1059554c77133317198332f895329c5eb43a
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-11 12:59:59 +0200

    Update app.js

--- exercices-dom/app.js
+++ exercices-dom/app.js
@@ -1,3 +1,23 @@
-// DOM - Document Object Model
+// Exercice 1
 
-// Style
+
+function message() {
+    alert("C'est cool hein!?");
+}
+
+// Exercice 2
+
+function afficheMessage() {
+    alert(document.getElementById('exo2').value);
+}
+
+//Exercice 3
+
+const body = document.body;
+
+function changeAmbiance() {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+}
+
+//Exercice 4
