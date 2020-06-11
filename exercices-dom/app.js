6a77aca6bea006624b5bf2d7f311e1a5d0f3dbe8
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-11 10:49:15 +0200

    Update app.js

--- exercices-dom/app.js
+++ exercices-dom/app.js
@@ -1,19 +1,3 @@
 // DOM - Document Object Model
 
 // Style
-
-let liste;
-
-// liste = document.querySelector('li');
-// liste = document.querySelector('li:nth(3)');
-
-// liste.style.color = "red";
-// liste.style.backgroundColor = "blue";
-
-liste = document.querySelectorAll('ul li');
-liste[1].getElementsByClassName.color = "red";
-
-for (var i = 0; i < liste.length; i++) {
-    // liste[i].style.backgroundColor = "blue";
-
-}
