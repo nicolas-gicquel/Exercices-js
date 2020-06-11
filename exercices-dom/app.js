0499b7dd43fecafb4ed277000e0795f6bd1baa16
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-11 16:43:45 +0200

    Update app.js

--- exercices-dom/app.js
+++ exercices-dom/app.js
@@ -7,17 +7,37 @@ function message() {
 
 // Exercice 2
 
+//Avec la fonction getElementById()
 function afficheMessage() {
     alert(document.getElementById('exo2').value);
 }
 
+//Avec la fonction getElementByClassName()
+function afficheMessage() {
+    alert(document.getElementsByClassName('exo2')[0].value);
+}
+
 //Exercice 3
 
-const body = document.body;
+const page = document.body;
+
 
 function changeAmbiance() {
-    body.style.backgroundColor = "black";
-    body.style.color = "white";
+    page.style.backgroundColor = "black";
+    page.style.color = "white";
 }
 
 //Exercice 4
+
+function modifyImage(img) {
+    // setTimeout(function() { document.getElementById('imgChange').src = img; }, 500);
+    document.getElementById('imgChange').src = img;
+}
+
+//Exercice 5
+
+function scrollImage() {
+    console.log('truc');
+    const pano = document.getElementById(imgPano);
+    pano.classList.remove("#imgPano");
+}
