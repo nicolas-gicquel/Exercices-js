21e44b91a8d6e6d03a74035d589eeb4031d2033f
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-15 16:47:08 +0200

    Create app.js

---
+++ exercices-jquery/app.js
@@ -0,0 +1,10 @@
+//Exercice 1 //
+console.log('test');
+
+$(window).load(function() {
+    $('#exercice_1').click(function() {
+        // $('.b1e1').hide();
+        console.log('test');
+    });
+
+});
